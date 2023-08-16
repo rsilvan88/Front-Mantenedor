0aba64b6b995b317efa5ffd945649868e13e5129
rsilvan88 <rsilvan@live.cl>
2023-08-15 22:05:59 -0400

    Update lista-tareas.component.spec.ts

--- src/app/lista-tareas/lista-tareas.component.spec.ts
+++ src/app/lista-tareas/lista-tareas.component.spec.ts
@@ -1,21 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ListaTareasComponent } from './lista-tareas.component';
 import { TareaService } from '../tarea.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Tarea } from '../tarea';
+import { of } from 'rxjs';
+
 
 describe('ListaTareasComponent', () => {
   let service: TareaService;
+  let httpClientSpy: {get: jasmine.Spy};
+
   beforeEach(async () => {
-     await TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule],
-      declarations: [ListaTareasComponent],
-    }).compileComponents();
-    service = TestBed.inject(TareaService)
+     httpClientSpy = jasmine.createSpyObj('HttpClient',['get']);
+     service = new TareaService(httpClientSpy as any)
   });
 
-  it('should create the ListarComponente Service', () => {
+  it('Servicio debe crearse correctamente', () => {
     expect(service).toBeTruthy();
   });
-    
-});
+
+  it('Se debe listar un array de tareas', () => {
+
+     const listTareas: Tarea[] = [{
+      identificador: 1,
+      descripcion: "Tarea de Ciencias",
+      fechaCreacion: new Date(),
+      vigente: true,
+      }, {
+      identificador: 3,
+      descripcion: "Tarea de Proyecto",
+      fechaCreacion: new Date(),
+      vigente: true,
+  }];
+
+  const mockResultadoList = [
+    {
+        "identificador": 1,
+        "descripcion": "Tarea de Ciencias",
+        "fechaCreacion": new Date(),
+        "vigente": true
+    },
+    {
+        "identificador": 3,
+        "descripcion": "Tarea de Proyecto",
+        "fechaCreacion": new Date(),
+        "vigente": true
+    }
+];
+
+    httpClientSpy.get.and.returnValue(of(mockResultadoList));
+
+    service.obtenerListaTareas().subscribe((data) =>{
+      expect(data).toEqual(listTareas);
+  
+  });
+  expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
+
+})});
